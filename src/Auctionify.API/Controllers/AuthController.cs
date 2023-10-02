97fa62c867828b340d1a219efa8bb359bfb3cff6
otash8110 <otash_baxadirov@mail.ru>
2023-10-02 14:32:55 +0500

    [79] - Add auth controller base

---
+++ src/Auctionify.API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auctionify.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AuthController : Controller
+    {
+        
+    }
+}
