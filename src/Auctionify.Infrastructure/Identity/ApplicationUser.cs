61ab53869d83ef7de32ae89008f2af0034c8125d
otash8110 <otash_baxadirov@mail.ru>
2023-10-02 13:46:53 +0500

    [79] - Add AppUser and IdentityDbContext base

---
+++ src/Auctionify.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Auctionify.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ src/Auctionify.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Auctionify.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctionify.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
