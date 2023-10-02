9a0c9ad66a64d0ef71ee6be4747d2c1b0302fcf2
otash8110 <otash_baxadirov@mail.ru>
2023-10-02 14:25:52 +0500

    [79] - Add Interface and Implementation for IdentityService

---
+++ src/Auctionify.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,11 @@
+﻿namespace Auctionify.Application.Common.Interfaces
+{
+    /// <summary>
+    /// Provides an abstraction IdentityService needed for user Authentication/Authorization process
+    /// </summary>
+    /// Add your corresponding method here like LoginAsync, RegisterAsync and etc.
+    public interface IIdentityService
+    {
+        
+    }
+}
