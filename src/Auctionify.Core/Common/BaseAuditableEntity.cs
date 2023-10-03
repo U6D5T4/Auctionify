e01fb08c863abb5775c033ea980a3e477cf0b299
otash8110 <otash_baxadirov@mail.ru>
2023-10-03 17:32:56 +0500

    [56] - Add auditable base entities and events

---
+++ src/Auctionify.Core/Common/BaseAuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auctionify.Core.Common
+{
+    internal class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
---
+++ src/Auctionify.Core/Common/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Auctionify.Core.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        private readonly List<BaseEvent> domainEvents = new();
+
+        [NotMapped]
+        public IReadOnlyCollection<BaseEvent> DomainEvents => domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(BaseEvent domainEvent)
+        {
+            domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(BaseEvent domainEvent)
+        {
+            domainEvents.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            domainEvents.Clear();
+        }
+    }
+}
