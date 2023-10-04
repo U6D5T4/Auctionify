9ea19c595e5d7b90d5721b374c776b697b186773
Отабек Бачадыров <otash_baxadirov@mail.ru>
2023-10-04 18:38:55 +0500

    [56] - Remove DataAnnotasions from User

--- src/Auctionify.Core/Entities/User.cs
+++ src/Auctionify.Core/Entities/User.cs
@@ -4,10 +4,8 @@ namespace Auctionify.Core.Entities
 {
     public class User : IdentityUser<int>
     {
-        [MaxLength(50)]
         public string? FirstName { get; set; }
 
-        [MaxLength(50)]
         public string? LastName { get; set; }
 
     }
