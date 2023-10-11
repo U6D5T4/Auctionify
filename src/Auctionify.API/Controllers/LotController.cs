ac5af931d69ded78d141e12f7c0ad2e1e3833005
otash8110 <otash_baxadirov@mail.ru>
2023-10-11 23:34:15 +0500

    [83] - Add Post Lot Endpoint and implement CreateLotCommand

--- src/Auctionify.API/Controllers/LotController.cs
+++ src/Auctionify.API/Controllers/LotController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Auctionify.Application.Features.Lots.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Auctionify.API.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/v1/[controller]")]
 	[ApiController]
 	public class LotController : ControllerBase
 	{
+		private readonly IMediator mediator;
 
+		public LotController(IMediator mediator)
+		{
+			this.mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateLot(CreateLotCommand createLotCommand)
+		{
+			var result = await mediator.Send(createLotCommand);
+
+			return Ok(result);
+		}
 	}
 }
--- src/Auctionify.Application/Features/Lots/Commands/Create/CreateLotCommand.cs
+++ src/Auctionify.Application/Features/Lots/Commands/Create/CreateLotCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Auctionify.Application.Common.Interfaces.Repositories;
 using Auctionify.Core.Entities;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 
@@ -11,36 +12,74 @@ namespace Auctionify.Application.Features.Lots.Commands.Create
 
         public string Description { get; set; }
 
-        public decimal StartingPrice { get; set; }
+        public decimal? StartingPrice { get; set; }
 
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
 
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
 
-        public Location Location { get; set; }
+        public LocationDto Location { get; set; }
 
-        public int CurrencyId { get; set; }
+        public int? CurrencyId { get; set; }
 
-        public IList<IFormFile> Photos { get; set; }
+        public IList<IFormFile>? Photos { get; set; }
 
-        public IList<IFormFile> AdditionalDocuments {  get; set; }
+        public IList<IFormFile>? AdditionalDocuments {  get; set; }
 
+        public bool IsDraft { get; set; }
+
+    }
+
+    public class LocationDto
+    {
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string Address { get; set; }
     }
 
     public class CreateLotCommandHandler : IRequestHandler<CreateLotCommand, CreatedLotResponse>
     {
-        private readonly ICategoryRepository categoryRepository;
+        private readonly ILotRepository lotRepository;
+        private readonly IMapper mapper;
 
-        public CreateLotCommandHandler(ICategoryRepository categoryRepository)
+        public CreateLotCommandHandler(ILotRepository lotRepository,
+            IMapper mapper)
         { 
-            this.categoryRepository = categoryRepository;
+            this.lotRepository = lotRepository;
+            this.mapper = mapper;
         }
 
-        public Task<CreatedLotResponse> Handle(CreateLotCommand request, CancellationToken cancellationToken)
+        public async Task<CreatedLotResponse> Handle(CreateLotCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var location = new Location
+            {
+                Address = request.Location.Address,
+                City = request.Location.City,
+                State = request.Location.State,
+                Country = request.Location.Country,
+            };
+
+            var lot = new Lot
+            {
+                Title = request.Title,
+                Description = request.Description,
+                StartingPrice = request.StartingPrice,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+                CategoryId = request.CategoryId,
+                Location = location,
+                CurrencyId = request.CurrencyId,
+            };
+
+            var createdLot = await lotRepository.AddAsync(lot);
+
+            return mapper.Map<CreatedLotResponse>(createdLot);
         }
     }
 }
