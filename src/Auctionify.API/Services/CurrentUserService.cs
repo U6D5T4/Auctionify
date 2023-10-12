11241252410bf3807b5128f02ac732831284d872
otash8110 <otash_baxadirov@mail.ru>
2023-10-12 19:01:52 +0500

    [83] - Add CurrentUserService

---
+++ src/Auctionify.API/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using Auctionify.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace Auctionify.API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserEmail => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+    }
+}
