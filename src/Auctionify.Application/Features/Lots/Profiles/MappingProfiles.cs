6ebe338bd49296ea2b5cc739a98ead83ac390393
otash8110 <otash_baxadirov@mail.ru>
2023-10-17 12:31:48 +0500

    [91] - Add Mapping to GetAllLots

--- src/Auctionify.Application/Features/Lots/Profiles/MappingProfiles.cs
+++ src/Auctionify.Application/Features/Lots/Profiles/MappingProfiles.cs
@@ -1,5 +1,6 @@
 ﻿using Auctionify.Application.Features.Lots.Commands.Create;
 using Auctionify.Application.Features.Lots.Commands.Update;
+using Auctionify.Application.Features.Lots.Queries.GetAllLots;
 using Auctionify.Application.Features.Lots.Queries.GetById;
 using Auctionify.Core.Entities;
 using AutoMapper;
@@ -10,7 +11,8 @@ namespace Auctionify.Application.Features.Lots.Profiles
 	{
 		public MappingProfiles() 
 		{
-			CreateMap<Lot, CreatedLotResponse>().ReverseMap();
+            CreateMap<Lot, GetAllLotsResponse>().ReverseMap();
+            CreateMap<Lot, CreatedLotResponse>().ReverseMap();
 			CreateMap<Lot, GetByIdLotResponse>().ReverseMap();
 			CreateMap<Lot, UpdateLotResponse>().ReverseMap();
 		}
