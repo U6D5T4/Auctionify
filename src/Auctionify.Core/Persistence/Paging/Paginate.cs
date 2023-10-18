921ed47a3e4914288df5c72b7e97731383693864
otash8110 <otash_baxadirov@mail.ru>
2023-10-18 23:55:45 +0500

    [97] - Add Base Pagination models to Core

---
+++ src/Auctionify.Core/Persistence/Paging/Paginate.cs
@@ -0,0 +1,49 @@
+﻿namespace Auctionify.Core.Persistence.Paging
+{
+    public class Paginate<T> : IPaginate<T>
+    {
+        public Paginate(IEnumerable<T> source, int index, int size, int from)
+        {
+            if (from > index)
+                throw new ArgumentException($"indexFrom: {from} > pageIndex: {index}, must indexFrom <= pageIndex");
+
+            Index = index;
+            Size = size;
+            From = from;
+            Pages = (int)Math.Ceiling(Count / (double)Size);
+
+            if (source is IQueryable<T> queryable)
+            {
+                Count = queryable.Count();
+                Items = queryable.Skip((Index - From) * Size).Take(Size).ToList();
+            }
+            else
+            {
+                T[] enumerable = source as T[] ?? source.ToArray();
+                Count = enumerable.Count();
+                Items = enumerable.Skip((Index - From) * Size).Take(Size).ToList();
+            }
+        }
+
+        public Paginate()
+        {
+            Items = Array.Empty<T>();
+        }
+
+        public int From { get; set; }
+
+        public int Index { get; set; }
+
+        public int Size { get; set; }
+
+        public int Count { get; set; }
+
+        public int Pages { get; set; }
+
+        public IList<T> Items { get; set; }
+
+        public bool HasPrevious => Index - From > 0;
+
+        public bool HasNext => Index - From + 1 < Pages;
+    }
+}
