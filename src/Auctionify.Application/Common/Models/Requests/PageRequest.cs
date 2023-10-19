c930bff7b8be457926c3aca6b81a4b9551209382
Отабек Бачадыров <otash_baxadirov@mail.ru>
2023-10-19 16:16:31 +0500

    [97] - Add PageRequest base class

---
+++ src/Auctionify.Application/Common/Models/Requests/PageRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Auctionify.Application.Common.Models.Requests
+{
+    public class PageRequest
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
