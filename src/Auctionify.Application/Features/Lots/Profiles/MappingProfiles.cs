9fb4b477079e1a180c48eef6bedfb65ce642cacc
otash8110 <otash_baxadirov@mail.ru>
2023-10-19 12:34:19 +0500

    [97] - Update mappings

--- src/Auctionify.Application/Features/Lots/Profiles/MappingProfiles.cs
+++ src/Auctionify.Application/Features/Lots/Profiles/MappingProfiles.cs
@@ -1,8 +1,11 @@
-﻿using Auctionify.Application.Features.Lots.Commands.Create;
+﻿using Auctionify.Application.Common.DTOs;
+using Auctionify.Application.Features.Lots.Commands.Create;
 using Auctionify.Application.Features.Lots.Commands.Delete;
 using Auctionify.Application.Features.Lots.Queries.GetAll;
+using Auctionify.Application.Features.Lots.Queries.GetAllByName;
 using Auctionify.Application.Features.Lots.Queries.GetById;
 using Auctionify.Core.Entities;
+using Auctionify.Core.Persistence.Paging;
 using AutoMapper;
 
 namespace Auctionify.Application.Features.Lots.Profiles
@@ -15,6 +18,9 @@ namespace Auctionify.Application.Features.Lots.Profiles
             CreateMap<Lot, CreatedLotResponse>().ReverseMap();
 			CreateMap<Lot, GetByIdLotResponse>().ReverseMap();
 			CreateMap<Lot, DeletedLotResponse>().ReverseMap();
+			CreateMap<Lot, GetAllLotsByNameResponse>().ReverseMap();
+
+			CreateMap<IPaginate<Lot>, GetListResponseDto<GetAllLotsByNameResponse>>().ReverseMap();
 		}
 	}
 }
