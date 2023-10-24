e13cca2c60dafee201bf8a0aa4aa63d214aa0a5c
SaidganiUsm <saidgani110@gmail.com>
2023-10-24 09:55:22 +0500

    [99] BaseModel for GetAll method's response

--- src/Auctionify.Application/Features/Lots/Queries/GetAll/GetAllLotsResponse.cs
+++ src/Auctionify.Application/Features/Lots/Queries/GetAll/GetAllLotsResponse.cs
@@ -1,21 +1,9 @@
-﻿namespace Auctionify.Application.Features.Lots.Queries.GetAll
+﻿using Auctionify.Application.Features.Lots.Queries.BaseQueryModels;
+
+namespace Auctionify.Application.Features.Lots.Queries.GetAll
 {
-    public class GetAllLotsResponse
+    public class GetAllLotsResponse : GetAllLots
     {
-        public int Id { get; set; }
-
-        public int CategoryId { get; set; }
-
-        public int LocationId { get; set; }
-
-        public int CurrencyId { get; set; }
-
-        public string Title { get; set; }
-
-        public decimal StartingPrice { get; set; }
-
-        public DateTime StartDate { get; set; }
-
-        public DateTime EndDate { get; set; }
+       
     }
 }
--- src/Auctionify.Application/Features/Lots/Queries/GetAllByLocation/GetAllLotsByLocationResponse.cs
+++ src/Auctionify.Application/Features/Lots/Queries/GetAllByLocation/GetAllLotsByLocationResponse.cs
@@ -1,31 +1,10 @@
 ﻿using Auctionify.Application.Common.DTOs;
+using Auctionify.Application.Features.Lots.Queries.BaseQueryModels;
 
 namespace Auctionify.Application.Features.Lots.Queries.GetAllByName
 {
-    public class GetAllLotsByLocationResponse
+    public class GetAllLotsByLocationResponse : GetAllLots
     {
-        public int Id { get; set; }
-
-        public string Title { get; set; }
-
-        public string Description { get; set; }
-
-        public decimal? StartingPrice { get; set; }
-
-        public DateTime StartDate { get; set; }
-
-        public DateTime EndDate { get; set; }
-
         public UserDto Seller { get; set; }
-
-        public CategoryDto Category { get; set; }
-
-        public LotStatusDto LotStatus { get; set; }
-
-        public LocationDto Location { get; set; }
-
-        public CurrencyDto Currency { get; set; }
-
-        public ICollection<BidDto> Bids { get; set; }
     }
 }
