4b845017cd8f96569cd7e39aba5fbb6dd9d08aaa
otash8110 <otash_baxadirov@mail.ru>
2023-10-27 00:32:56 +0500

    [108] - Refactor FilterLotsQuery and add pagination to it

--- src/Auctionify.Application/Features/Lots/Queries/FIlter/FilterLotsQuery.cs
+++ src/Auctionify.Application/Features/Lots/Queries/FIlter/FilterLotsQuery.cs
@@ -1,6 +1,9 @@
 ﻿using Auctionify.Application.Common.DTOs;
 using Auctionify.Application.Common.Interfaces.Repositories;
+using Auctionify.Application.Common.Models.Requests;
+using Auctionify.Core.Entities;
 using Auctionify.Core.Persistence.Dynamic;
+using Auctionify.Core.Persistence.Paging;
 using AutoMapper;
 using MediatR;
 using System.Linq;
@@ -17,7 +20,11 @@ namespace Auctionify.Application.Features.Lots.Queries.FIlter
 
         public IList<int>? LotStatuses { get; set; }
 
-        public string? Sort { get; set; }
+        public string? SortField { get; set; }
+
+        public string? SortDir { get; set; }
+
+        public PageRequest? PageRequest { get; set; }
     }
 
     public class FilterLotsQueryHandler : IRequestHandler<FilterLotsQuery, GetListResponseDto<FilterLotsResponse>>
@@ -25,6 +32,7 @@ namespace Auctionify.Application.Features.Lots.Queries.FIlter
         private string startingPriceField = "StartingPrice";
         private string categoryField = "CategoryId";
         private string lotStatusField = "LotStatusId";
+        private string defaultOrder = "asc";
         private readonly ILotRepository _lotRepository;
         private readonly IMapper _mapper;
 
@@ -47,74 +55,13 @@ namespace Auctionify.Application.Features.Lots.Queries.FIlter
 
             if (request.LotStatuses != null)
             {
-                var statusFiltersBase = new Filter
-                {
-                    Field = lotStatusField,
-                    Logic = "and",
-                    Operator = "isnotnull",
-                    Filters = new List<Filter>(),
-                };
-
-                Filter? localFilter = null;
-
-                for (int i = 0; i < request.LotStatuses.Count; i++)
-                {
-                    var statusFilter = new Filter
-                    {
-                        Filters = new List<Filter>()
-                    };
-
-                    statusFilter.Field = lotStatusField;
-                    statusFilter.Logic = "or";
-                    statusFilter.Value = request.LotStatuses[i].ToString();
-                    statusFilter.Operator = "eq";
-
-                    if (localFilter != null) statusFilter.Filters.Add(localFilter);
-
-                    localFilter = statusFilter;
-                }
-                statusFiltersBase.Filters.Add(localFilter);
+                var statusFiltersBase = CreateStatusFilter(request.LotStatuses, lotStatusField);
                 filterBase.Filters.Add(statusFiltersBase);
             }
 
             if (request.MinimumPrice != null || request.MaximumPrice != null)
             {
-                var priceBaseFilter = new Filter
-                {
-                    Field = startingPriceField,
-                    Operator = "isnotnull",
-                    Logic = "and",
-                    Filters = new List<Filter>()
-                };
-
-                if (request.MinimumPrice != null)
-                {
-                    var filterMiniumPrice = new Filter
-                    {
-                        Field = startingPriceField,
-                        Value = request.MinimumPrice.ToString(),
-                        Operator = "gte",
-                        Logic = "and",
-                        Filters = new List<Filter>()
-                    };
-
-                    priceBaseFilter.Filters.Add(filterMiniumPrice);
-                }
-
-                if (request.MaximumPrice != null)
-                {
-                    var filterMaximumPrice = new Filter
-                    {
-                        Field = startingPriceField,
-                        Value = request.MaximumPrice.ToString(),
-                        Operator = "lte",
-                        Logic = "and",
-                        Filters = new List<Filter>()
-                    };
-
-                    priceBaseFilter.Filters.Add(filterMaximumPrice);
-                }
-
+                var priceBaseFilter = CreatePriceFilter(request.MinimumPrice, request.MaximumPrice, startingPriceField);
                 filterBase.Filters.Add(priceBaseFilter);
             }
 
@@ -132,14 +79,99 @@ namespace Auctionify.Application.Features.Lots.Queries.FIlter
                 filterBase.Filters.Add(filterCategory);
             }
 
+            Sort dynamicSort = null;
+
+            if (!string.IsNullOrEmpty(request.SortField))
+            {
+                dynamicSort = new Sort
+                {
+                    Field = request.SortField,
+                    Dir = !string.IsNullOrEmpty(request.SortDir) ? request.SortDir : defaultOrder
+                };
+            }
+
             var dynamicQuery = new DynamicQuery
             {
                 Filter = filterBase,
+                Sort = dynamicSort != null ? new List<Sort> { dynamicSort } : null,
             };
 
-            var result = await _lotRepository.GetListByDynamicAsync(dynamicQuery);
+            var result = await (request.PageRequest != null
+                ? _lotRepository.GetListByDynamicAsync(dynamicQuery, index: request.PageRequest.PageIndex, size: request.PageRequest.PageSize)
+                : _lotRepository.GetListByDynamicAsync(dynamicQuery));
 
             return _mapper.Map<GetListResponseDto<FilterLotsResponse>>(result);
+        }
+
+        private Filter CreateStatusFilter(IList<int> statuses, string field)
+        {
+            var statusFiltersBase = new Filter
+            {
+                Field = lotStatusField,
+                Logic = "and",
+                Operator = "isnotnull",
+                Filters = new List<Filter>(),
+            };
+
+            Filter? localFilter = null;
+
+            for (int i = 0; i < statuses.Count; i++)
+            {
+                var statusFilter = new Filter
+                {
+                    Filters = new List<Filter>()
+                };
+
+                statusFilter.Field = lotStatusField;
+                statusFilter.Logic = "or";
+                statusFilter.Value = statuses[i].ToString();
+                statusFilter.Operator = "eq";
+
+                if (localFilter != null) statusFilter.Filters.Add(localFilter);
+
+                localFilter = statusFilter;
+            }
+
+            statusFiltersBase.Filters.Add(localFilter);
+
+            return statusFiltersBase;
+        }
+
+        Filter CreatePriceFilter(decimal? minPrice, decimal? maxPrice, string field)
+        {
+            var priceBaseFilter = new Filter
+            {
+                Field = field,
+                Operator = "isnotnull",
+                Logic = "and",
+                Filters = new List<Filter>()
+            };
+
+            if (minPrice != null)
+            {
+                priceBaseFilter.Filters.Add(new Filter
+                {
+                    Field = field,
+                    Value = minPrice.ToString(),
+                    Operator = "gte",
+                    Logic = "and",
+                    Filters = new List<Filter>()
+                });
+            }
+
+            if (maxPrice != null)
+            {
+                priceBaseFilter.Filters.Add(new Filter
+                {
+                    Field = field,
+                    Value = maxPrice.ToString(),
+                    Operator = "lte",
+                    Logic = "and",
+                    Filters = new List<Filter>()
+                });
+            }
+
+            return priceBaseFilter;
         }
     }
 }
