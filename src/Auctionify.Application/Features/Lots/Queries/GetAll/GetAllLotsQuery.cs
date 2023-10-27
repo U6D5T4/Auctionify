c6884ffdd47b47f5739b6148fc858880194627b6
Khasan Rashidov <khasanrashidov2002@gmail.com>
2023-10-27 19:28:13 +0500

    [100] corrections on getalllots query with main photos (blob)

--- src/Auctionify.Application/Features/Lots/Queries/GetAll/GetAllLotsQuery.cs
+++ src/Auctionify.Application/Features/Lots/Queries/GetAll/GetAllLotsQuery.cs
@@ -2,33 +2,51 @@
 using Auctionify.Application.Common.Interfaces.Repositories;
 using AutoMapper;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace Auctionify.Application.Features.Lots.Queries.GetAll
 {
-    public class GetAllLotsQuery : IRequest<List<GetAllLotsResponse>>
-    {
+	public class GetAllLotsQuery : IRequest<List<GetAllLotsResponse>>
+	{
 
-    }
+	}
 
-    public class GetAllLotsQueryHandler : IRequestHandler<GetAllLotsQuery, List<GetAllLotsResponse>>
-    {
-        private readonly ILotRepository _lotRepository;
-        private readonly IMapper _mapper;
+	public class GetAllLotsQueryHandler : IRequestHandler<GetAllLotsQuery, List<GetAllLotsResponse>>
+	{
+		private readonly ILotRepository _lotRepository;
+		private readonly IMapper _mapper;
+		private readonly IFileRepository _fileRepository;
+		private readonly IBlobService _blobService;
 
-        public GetAllLotsQueryHandler(ILotRepository lotRepository, IMapper mapper)
-        {
-            _lotRepository = lotRepository;
-            _mapper = mapper;
-        }
+		public GetAllLotsQueryHandler(ILotRepository lotRepository, IMapper mapper, IFileRepository fileRepository, IBlobService blobService)
+		{
+			_lotRepository = lotRepository;
+			_mapper = mapper;
+			_fileRepository = fileRepository;
+			_blobService = blobService;
+		}
 
-        public async Task<List<GetAllLotsResponse>> Handle(GetAllLotsQuery request, CancellationToken cancellationToken)
-        {
-            var lots = await _lotRepository.GetListAsync(cancellationToken: cancellationToken);
+		public async Task<List<GetAllLotsResponse>> Handle(GetAllLotsQuery request, CancellationToken cancellationToken)
+		{
+			var lots = await _lotRepository.GetListAsync(cancellationToken: cancellationToken);
 
-			var response = _mapper.Map<List<GetAllLotsResponse>>(lots);
+			var response = new List<GetAllLotsResponse>();
 
-            return response;
-        }
-    }
+			foreach (var lot in lots)
+			{
+				var photo = await _fileRepository.GetAsync(predicate: x => x.LotId == lot.Id && x.Path.Contains("photos/"), cancellationToken: cancellationToken);
+
+				var lotResponse = _mapper.Map<GetAllLotsResponse>(lot);
+
+				if (photo != null)
+				{
+					var linkToPhoto = _blobService.GetBlobUrl(photo.Path, photo.FileName);
+
+					lotResponse.MainPhoto = linkToPhoto;
+				}
+
+				response.Add(lotResponse);
+			}
+			return response;
+		}
+	}
 }
--- src/Auctionify.Application/Features/Lots/Queries/GetAll/GetAllLotsResponse.cs
+++ src/Auctionify.Application/Features/Lots/Queries/GetAll/GetAllLotsResponse.cs
@@ -1,21 +1,23 @@
 ﻿namespace Auctionify.Application.Features.Lots.Queries.GetAll
 {
-    public class GetAllLotsResponse
-    {
-        public int Id { get; set; }
+	public class GetAllLotsResponse
+	{
+		public int Id { get; set; }
 
-        public int CategoryId { get; set; }
+		public int CategoryId { get; set; }
 
-        public int LocationId { get; set; }
+		public int LocationId { get; set; }
 
-        public int CurrencyId { get; set; }
+		public int CurrencyId { get; set; }
 
-        public string Title { get; set; }
+		public string Title { get; set; }
 
-        public decimal StartingPrice { get; set; }
+		public decimal StartingPrice { get; set; }
 
-        public DateTime StartDate { get; set; }
+		public DateTime StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
-    }
+		public DateTime EndDate { get; set; }
+
+		public string MainPhoto { get; set; }
+	}
 }
