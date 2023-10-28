96dd17f7a5f3ba0581660685df4213bf34f57e9b
SaidganiUsm <saidgani110@gmail.com>
2023-10-28 20:51:57 +0500

    [114] Controller for user

---
+++ src/Auctionify.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Auctionify.Application.Features.Users.Queries.GetById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auctionify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            var result = await _mediator.Send(new GetByIdUserQuery { Id = id });
+
+            return Ok(result);
+        }
+    }
+}
