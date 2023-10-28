f4434a1d3e6d3bfe0c64d92708029297e43da592
SaidganiUsm <saidgani110@gmail.com>
2023-10-28 20:51:23 +0500

    [114] Response and Mapping

---
+++ src/Auctionify.Application/Features/Users/Profiles/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using Auctionify.Application.Features.Users.Queries.GetById;
+using Auctionify.Core.Entities;
+using AutoMapper;
+
+namespace Auctionify.Application.Features.Users.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<User, GetByIdUserResponse>().ReverseMap();
+        }
+    }
+}
---
+++ src/Auctionify.Application/Features/Users/Queries/GetById/GetByIdUserResponse.cs
@@ -0,0 +1,15 @@
+﻿using Auctionify.Application.Common.DTOs;
+
+namespace Auctionify.Application.Features.Users.Queries.GetById
+{
+    public class GetByIdUserResponse
+    {
+        public string Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public ICollection<LotDto> SellingLots { get; set; }
+
+        public ICollection<RateDto> ReceiverRates { get; set; }
+    }
+}
