357b2b6b6d3a63e7dd7102443383ca13f78c70bf
Отабек Бачадыров <otash_baxadirov@mail.ru>
2023-11-01 18:58:52 +0500

    [105] - Refactor Lot Commands and Quries

--- src/Auctionify.Application/Features/Lots/Commands/Delete/DeleteLotCommand.cs
+++ src/Auctionify.Application/Features/Lots/Commands/Delete/DeleteLotCommand.cs
@@ -7,63 +7,62 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Auctionify.Application.Features.Lots.Commands.Delete
 {
-	public class DeleteLotCommand : IRequest<DeletedLotResponse>
-	{
-		public int Id { get; set; }
+    public class DeleteLotCommand : IRequest<DeletedLotResponse>
+    {
+        public int Id { get; set; }
 
-		public class DeleteLotCommandHandler : IRequestHandler<DeleteLotCommand, DeletedLotResponse>
-		{
-			private readonly IMapper _mapper;
-			private readonly ILotRepository _lotRepository;
-			private readonly ILotStatusRepository _lotStatusRepository;
-			private readonly IBidRepository _bidRepository;
+    }
+    public class DeleteLotCommandHandler : IRequestHandler<DeleteLotCommand, DeletedLotResponse>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILotRepository _lotRepository;
+        private readonly ILotStatusRepository _lotStatusRepository;
+        private readonly IBidRepository _bidRepository;
 
-			public DeleteLotCommandHandler(IMapper mapper, 
-										   ILotRepository lotRepository, 
-										   ILotStatusRepository lotStatusRepository, 
-										   IBidRepository bidRepository)
-			{
-				_mapper = mapper;
-				_lotRepository = lotRepository;
-				_lotStatusRepository = lotStatusRepository;
-				_bidRepository = bidRepository;
-			}
+        public DeleteLotCommandHandler(IMapper mapper,
+                                       ILotRepository lotRepository,
+                                       ILotStatusRepository lotStatusRepository,
+                                       IBidRepository bidRepository)
+        {
+            _mapper = mapper;
+            _lotRepository = lotRepository;
+            _lotStatusRepository = lotStatusRepository;
+            _bidRepository = bidRepository;
+        }
 
-			public async Task<DeletedLotResponse> Handle(DeleteLotCommand request, CancellationToken cancellationToken)
-			{
-				var lot = await _lotRepository.GetAsync(predicate: x => x.Id == request.Id, cancellationToken: cancellationToken, include: x => x.Include(x => x.LotStatus).Include(x => x.Bids));
+        public async Task<DeletedLotResponse> Handle(DeleteLotCommand request, CancellationToken cancellationToken)
+        {
+            var lot = await _lotRepository.GetAsync(predicate: x => x.Id == request.Id, cancellationToken: cancellationToken, include: x => x.Include(x => x.LotStatus).Include(x => x.Bids));
 
-				var currentLotStatus = lot!.LotStatus.Name;
+            var currentLotStatus = lot!.LotStatus.Name;
 
-				var newLotStatus = await _lotStatusRepository.GetAsync(predicate: s => s.Name == AuctionStatus.Cancelled.ToString(), cancellationToken: cancellationToken);
+            var newLotStatus = await _lotStatusRepository.GetAsync(predicate: s => s.Name == AuctionStatus.Cancelled.ToString(), cancellationToken: cancellationToken);
 
-				if (currentLotStatus == AuctionStatus.Active.ToString())
-				{
-					lot!.LotStatusId = newLotStatus!.Id;
+            if (currentLotStatus == AuctionStatus.Active.ToString())
+            {
+                lot!.LotStatusId = newLotStatus!.Id;
 
-					if (lot!.Bids.Any())
-						await _bidRepository.DeleteRangeAsync(lot!.Bids.ToList());
+                if (lot!.Bids.Any())
+                    await _bidRepository.DeleteRangeAsync(lot!.Bids.ToList());
 
-					await _lotRepository.UpdateAsync(lot!);
+                await _lotRepository.UpdateAsync(lot!);
 
-					var updateStatusResponse = _mapper.Map<DeletedLotResponse>(lot);
+                var updateStatusResponse = _mapper.Map<DeletedLotResponse>(lot);
 
-					updateStatusResponse.WasDeleted = false;
+                updateStatusResponse.WasDeleted = false;
 
-					return updateStatusResponse;
-				}
+                return updateStatusResponse;
+            }
 
-				await _lotRepository.DeleteAsync(lot!);
+            await _lotRepository.DeleteAsync(lot!);
 
-				bool wasDeleted = true;
+            bool wasDeleted = true;
 
-				var response = _mapper.Map<DeletedLotResponse>(lot);
+            var response = _mapper.Map<DeletedLotResponse>(lot);
 
-				response.WasDeleted = wasDeleted;
+            response.WasDeleted = wasDeleted;
 
-				return response;
-			}
-		}
-
-	}
+            return response;
+        }
+    }
 }
--- src/Auctionify.Application/Features/Lots/Queries/GetById/GetByIdLotQuery.cs
+++ src/Auctionify.Application/Features/Lots/Queries/GetById/GetByIdLotQuery.cs
@@ -8,25 +8,24 @@ namespace Auctionify.Application.Features.Lots.Queries.GetById
 	public class GetByIdLotQuery: IRequest<GetByIdLotResponse>
 	{
 		public int Id { get; set; }
-
-		public class GetByIdLotQueryHandler : IRequestHandler<GetByIdLotQuery, GetByIdLotResponse>
-		{
-			private readonly ILotRepository _lotRepository;
-			private readonly IMapper _mapper;
-
-			public GetByIdLotQueryHandler(ILotRepository lotRepository, IMapper mapper)
-			{
-				_lotRepository = lotRepository;
-				_mapper = mapper;
-			}
-
-			public async Task<GetByIdLotResponse> Handle(GetByIdLotQuery request, CancellationToken cancellationToken)
-			{
-				var lot = await _lotRepository.GetAsync(predicate: x => x.Id == request.Id, include: x => x.Include(x => x.Category).Include(x => x.Currency).Include(x => x.Location).Include(x => x.LotStatus).Include(x => x.Bids));
-				var result = _mapper.Map<GetByIdLotResponse>(lot);
-				return result;
-			}
-		}
-
 	}
+
+    public class GetByIdLotQueryHandler : IRequestHandler<GetByIdLotQuery, GetByIdLotResponse>
+    {
+        private readonly ILotRepository _lotRepository;
+        private readonly IMapper _mapper;
+
+        public GetByIdLotQueryHandler(ILotRepository lotRepository, IMapper mapper)
+        {
+            _lotRepository = lotRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetByIdLotResponse> Handle(GetByIdLotQuery request, CancellationToken cancellationToken)
+        {
+            var lot = await _lotRepository.GetAsync(predicate: x => x.Id == request.Id, include: x => x.Include(x => x.Category).Include(x => x.Currency).Include(x => x.Location).Include(x => x.LotStatus).Include(x => x.Bids));
+            var result = _mapper.Map<GetByIdLotResponse>(lot);
+            return result;
+        }
+    }
 }
