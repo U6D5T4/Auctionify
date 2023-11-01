7bf6c8eb0222a31cf310050a517c47e24d6838ac
SaidganiUsm <saidgani110@gmail.com>
2023-11-01 19:14:11 +0500

    [116] (Front-End) Common layout

--- src/Auctionify.API/ClientApp/src/app/app.module.ts
+++ src/Auctionify.API/ClientApp/src/app/app.module.ts
@@ -8,18 +8,21 @@ import { ApiAuthorizationModule } from './api-authorization/api-authorization.mo
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthorizeInterceptor } from './api-authorization/authorize.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from './layout/layout.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ApiAuthorizationModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true}
