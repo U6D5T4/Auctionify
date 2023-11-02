27e97b7a7771af425062c7a25fd65978babd9058
otash8110 <otash_baxadirov@mail.ru>
2023-11-02 20:12:34 +0500

    [105] - Refactor CreateLot Tests and use memberData

--- tests/Auctionify.UnitTests/CreateLotTests/CreateLotsTests.cs
+++ tests/Auctionify.UnitTests/CreateLotTests/CreateLotsTests.cs
@@ -13,170 +13,159 @@ using System.Collections;
 
 namespace Auctionify.UnitTests.CreateLotTests
 {
-    public class CreateLotsTests
-    {
-        private readonly IMapper _mapper;
-        private readonly ILotRepository _lotRepository;
-        private readonly UserManager<User> _userManager;
-        private readonly ILotStatusRepository _lotStatusRepository;
-        private readonly ICurrentUserService _currentUserService;
-        private readonly CreateLotCommandValidator _validator;
+	public class CreateLotsTests
+	{
+		private readonly IMapper _mapper;
+		private readonly ILotRepository _lotRepository;
+		private readonly UserManager<User> _userManager;
+		private readonly ILotStatusRepository _lotStatusRepository;
+		private readonly ICurrentUserService _currentUserService;
+		private readonly CreateLotCommandValidator _validator;
+		private static readonly DateTime CustomDateTimeNow = new DateTime(2023, 11, 2, 13, 0, 0);
 
-        public CreateLotsTests() {
-            var mockDbContext = DbContextMock.GetMock<Lot, ApplicationDbContext>(new List<Lot>(), ctx => ctx.Lots);
-            mockDbContext = DbContextMock.GetMock(EntitiesSeeding.GetLotStatuses(), ctx => ctx.LotStatuses, mockDbContext);
-            mockDbContext = DbContextMock.GetMock(EntitiesSeeding.GetCategories(), ctx => ctx.Categories, mockDbContext);
-            mockDbContext = DbContextMock.GetMock(EntitiesSeeding.GetCurrencies(), ctx => ctx.Currency, mockDbContext);
 
-            _lotRepository = new LotRepository(mockDbContext.Object);
+		public CreateLotsTests()
+		{
+			var mockDbContext = DbContextMock.GetMock<Lot, ApplicationDbContext>(new List<Lot>(), ctx => ctx.Lots);
+			mockDbContext = DbContextMock.GetMock(EntitiesSeeding.GetLotStatuses(), ctx => ctx.LotStatuses, mockDbContext);
+			mockDbContext = DbContextMock.GetMock(EntitiesSeeding.GetCategories(), ctx => ctx.Categories, mockDbContext);
+			mockDbContext = DbContextMock.GetMock(EntitiesSeeding.GetCurrencies(), ctx => ctx.Currency, mockDbContext);
 
-            var configuration = new MapperConfiguration(cfg => cfg.AddProfiles(new List<Profile>
-            {
-                new Application.Common.Profiles.MappingProfiles(),
-                new Application.Features.Lots.Profiles.MappingProfiles(),
-            }));
-            _mapper = new Mapper(configuration);
+			_lotRepository = new LotRepository(mockDbContext.Object);
 
-            var currentUserService = new Mock<ICurrentUserService>();
-            currentUserService.Setup(x => x.UserEmail).Returns(It.IsAny<string>());
-            _currentUserService = currentUserService.Object;
+			var configuration = new MapperConfiguration(cfg => cfg.AddProfiles(new List<Profile>
+			{
+				new Application.Common.Profiles.MappingProfiles(),
+				new Application.Features.Lots.Profiles.MappingProfiles(),
+			}));
+			_mapper = new Mapper(configuration);
 
-            _lotStatusRepository = new LotStatusRepository(mockDbContext.Object);
+			var currentUserService = new Mock<ICurrentUserService>();
+			currentUserService.Setup(x => x.UserEmail).Returns(It.IsAny<string>());
+			_currentUserService = currentUserService.Object;
 
-            _userManager = EntitiesSeeding.GetUserManagerMock();
+			_lotStatusRepository = new LotStatusRepository(mockDbContext.Object);
 
-            var categoryRepository = new CategoryRepository(mockDbContext.Object);
-            var currencyRepository = new CurrencyRepository(mockDbContext.Object);
+			_userManager = EntitiesSeeding.GetUserManagerMock();
 
-            _validator = new CreateLotCommandValidator(categoryRepository, _lotStatusRepository, currencyRepository);
-        }
+			var categoryRepository = new CategoryRepository(mockDbContext.Object);
+			var currencyRepository = new CurrencyRepository(mockDbContext.Object);
 
-        [Fact]
-        public async Task CreateLotCommandHandler_WhenCalledAsDraft_ReturnsDraftLot()
-        {
-            var newLot = new CreateLotCommand
-            {
-                Title = "Testasdasd",
-                Description = "Descriptionasdasdadasdasdasdasdadaasdad",
-                Address = "Test address",
-                City = "Test city",
-                Country = "Test country",
-                IsDraft = true,
-            };
-            await _validator.TestValidateAsync(newLot);
-            var command = new CreateLotCommandHandler(_lotRepository, _lotStatusRepository, _currentUserService, _userManager, _mapper);
+			_validator = new CreateLotCommandValidator(categoryRepository, _lotStatusRepository, currencyRepository);
+		}
 
-            var result = await command.Handle(newLot, default);
+		[Fact]
+		public async Task CreateLotCommandHandler_WhenCalledAsDraft_ReturnsDraftLot()
+		{
+			var newLot = new CreateLotCommand
+			{
+				Title = "Testasdasd",
+				Description = "Descriptionasdasdadasdasdasdasdadaasdad",
+				Address = "Test address",
+				City = "Test city",
+				Country = "Test country",
+				IsDraft = true,
+			};
+			await _validator.TestValidateAsync(newLot);
+			var command = new CreateLotCommandHandler(_lotRepository, _lotStatusRepository, _currentUserService, _userManager, _mapper);
 
-            result.Should().BeOfType<CreatedLotResponse>();
-        }
+			var result = await command.Handle(newLot, default);
 
-        [Fact]
-        public async Task CreateLotCommandHandler_WhenDraftCalledWithWrongProperties_ShouldHaveValidationErrors()
-        {
-            var newLot = new CreateLotCommand
-            {
-                Title = "short",
-                Description = "short",
-                StartingPrice = 0,
-                CategoryId = 4,
-                CurrencyId = 4,
-                IsDraft = true
-            };
+			result.Should().BeOfType<CreatedLotResponse>();
+		}
 
-            var result = await _validator.TestValidateAsync(newLot, default);
+		[Fact]
+		public async Task CreateLotCommandHandler_WhenDraftCalledWithWrongProperties_ShouldHaveValidationErrors()
+		{
+			var newLot = new CreateLotCommand
+			{
+				Title = "short",
+				Description = "short",
+				StartingPrice = 0,
+				CategoryId = 4,
+				CurrencyId = 4,
+				IsDraft = true
+			};
 
-            result.ShouldHaveValidationErrorFor(lot => lot.Title);
-            result.ShouldHaveValidationErrorFor(lot => lot.Description);
-            result.ShouldHaveValidationErrorFor(lot => lot.StartingPrice);
-            result.ShouldHaveValidationErrorFor(lot => lot.Address);
-            result.ShouldHaveValidationErrorFor(lot => lot.City);
-            result.ShouldHaveValidationErrorFor(lot => lot.Country);
-            result.ShouldHaveValidationErrorFor(lot => lot.CategoryId);
-            result.ShouldHaveValidationErrorFor(lot => lot.CurrencyId);
-        }
+			var result = await _validator.TestValidateAsync(newLot, default);
 
-        [Fact]
-        public async Task CreateLotCommandHandler_WhenActiveCalledWithWrongAndEmtpyProperties_ShouldHaveValidationErrors()
-        {
-            var newLot = new CreateLotCommand
-            {
-                Title = "TOO LONG TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITEL TITLE TITTLE TITLE",
-                Description = "short",
-                StartingPrice = 0,
-                CategoryId = 4,
-                CurrencyId = 4,
-                IsDraft = false
-            };
+			result.ShouldHaveValidationErrorFor(lot => lot.Title);
+			result.ShouldHaveValidationErrorFor(lot => lot.Description);
+			result.ShouldHaveValidationErrorFor(lot => lot.StartingPrice);
+			result.ShouldHaveValidationErrorFor(lot => lot.Address);
+			result.ShouldHaveValidationErrorFor(lot => lot.City);
+			result.ShouldHaveValidationErrorFor(lot => lot.Country);
+			result.ShouldHaveValidationErrorFor(lot => lot.CategoryId);
+			result.ShouldHaveValidationErrorFor(lot => lot.CurrencyId);
+		}
 
-            var result = await _validator.TestValidateAsync(newLot, default);
+		[Fact]
+		public async Task CreateLotCommandHandler_WhenActiveCalledWithWrongAndEmtpyProperties_ShouldHaveValidationErrors()
+		{
+			var newLot = new CreateLotCommand
+			{
+				Title = "TOO LONG TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITEL TITLE TITTLE TITLE",
+				Description = "short",
+				StartingPrice = 0,
+				CategoryId = 4,
+				CurrencyId = 4,
+				IsDraft = false
+			};
 
-            result.ShouldHaveValidationErrorFor(lot => lot.Title);
-            result.ShouldHaveValidationErrorFor(lot => lot.Description);
-            result.ShouldHaveValidationErrorFor(lot => lot.StartingPrice)
-                .WithErrorCode("GreaterThanValidator");
-            result.ShouldHaveValidationErrorFor(lot => lot.Address)
-                .WithErrorCode("NotEmptyValidator");
-            result.ShouldHaveValidationErrorFor(lot => lot.City)
-                .WithErrorCode("NotEmptyValidator");
-            result.ShouldHaveValidationErrorFor(lot => lot.Country)
-                .WithErrorCode("NotEmptyValidator");
-            result.ShouldHaveValidationErrorFor(lot => lot.CategoryId);
-            result.ShouldHaveValidationErrorFor(lot => lot.CurrencyId);
-            result.ShouldHaveValidationErrorFor(lot => lot.StartDate)
-                .WithErrorCode("NotEmptyValidator");
-            result.ShouldHaveValidationErrorFor(lot => lot.Photos)
-                .WithErrorCode("NotEmptyValidator");
-        }
+			var result = await _validator.TestValidateAsync(newLot, default);
 
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-        [InlineData(4)]
-        public async Task CreateLotCommandHandler_WhenActiveCalledWithWrongDateProperties_ShouldHaveValidationErrors
-            (int testCondition)
-        {
-            var startDate = DateTime.MinValue.AddDays(1);
-            var endDate = DateTime.MinValue;
+			result.ShouldHaveValidationErrorFor(lot => lot.Title);
+			result.ShouldHaveValidationErrorFor(lot => lot.Description);
+			result.ShouldHaveValidationErrorFor(lot => lot.StartingPrice)
+				.WithErrorCode("GreaterThanValidator");
+			result.ShouldHaveValidationErrorFor(lot => lot.Address)
+				.WithErrorCode("NotEmptyValidator");
+			result.ShouldHaveValidationErrorFor(lot => lot.City)
+				.WithErrorCode("NotEmptyValidator");
+			result.ShouldHaveValidationErrorFor(lot => lot.Country)
+				.WithErrorCode("NotEmptyValidator");
+			result.ShouldHaveValidationErrorFor(lot => lot.CategoryId);
+			result.ShouldHaveValidationErrorFor(lot => lot.CurrencyId);
+			result.ShouldHaveValidationErrorFor(lot => lot.StartDate)
+				.WithErrorCode("NotEmptyValidator");
+			result.ShouldHaveValidationErrorFor(lot => lot.Photos)
+				.WithErrorCode("NotEmptyValidator");
+		}
 
-            // Cases for dynamic DateTime.Now changes (Doesnot work with ClassData/MemberData when passed as argument to test)
-            if (testCondition == 1 )
-            {
-                
-            }
-            else if (testCondition == 2)
-            {
-                startDate = DateTime.Now;
-                endDate = DateTime.Now;
-            }
-            else if (testCondition == 3)
-            {
-                startDate = DateTime.Now.AddDays(1).AddSeconds(2);
-                endDate = DateTime.Now;
-            }
-            else if (testCondition == 4)
-            {
-                startDate = DateTime.Now.AddDays(1).AddSeconds(2);
-                endDate = DateTime.Now.AddHours(2);
-            }
+		[Theory]
+		[MemberData(nameof(WrongDateTimeToValidate))]
+		public async Task CreateLotCommandHandler_WhenActiveCalledWithWrongDateProperties_ShouldHaveValidationErrors
+			(DateTime startDate, DateTime endDate)
+		{
+			// Time: 2023-11-2T13:00:00
+			var dateToTestOver = new DateTime(2023, 11, 2, 13, 0, 0);
 
-            var newLot = new CreateLotCommand
-            {
-                StartDate = startDate,
-                EndDate = endDate,
-                IsDraft = false
-            };
+			var newLot = new CreateLotCommand
+			{
+				StartDate = startDate,
+				EndDate = endDate,
+				IsDraft = false
+			};
 
-            var result = await _validator.TestValidateAsync(newLot, default);
+			var result = await _validator.TestValidateAsync(newLot, default);
 
-            if (startDate < DateTime.Now.AddDays(1))
-            {
-                result.ShouldHaveValidationErrorFor(lot => lot.StartDate)
-                        .WithErrorCode("PredicateValidator");
-            }
-            result.ShouldHaveValidationErrorFor(lot => lot.EndDate)
-                .WithErrorCode("PredicateValidator");
-        }
-    }
+			if (startDate < dateToTestOver)
+			{
+				result.ShouldHaveValidationErrorFor(lot => lot.StartDate)
+						.WithErrorCode("PredicateValidator");
+			}
+			result.ShouldHaveValidationErrorFor(lot => lot.EndDate)
+				.WithErrorCode("PredicateValidator");
+		}
+
+		public static List<object[]> WrongDateTimeToValidate = new List<object[]>
+		{
+			new object[] { DateTime.MinValue.AddDays(1), DateTime.MinValue },
+			new object[] { CustomDateTimeNow, CustomDateTimeNow },
+			new object[] { CustomDateTimeNow.AddDays(1), CustomDateTimeNow },
+			new object[] { CustomDateTimeNow.AddDays(1), CustomDateTimeNow.AddHours(2) },
+		};
+
+
+	}
 }
