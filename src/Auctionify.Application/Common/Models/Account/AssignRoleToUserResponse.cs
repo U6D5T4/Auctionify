ca5ea2863c00c876d7126ba98bfd4cb55750d022
SaidganiUsm <saidgani110@gmail.com>
2023-11-02 21:43:15 +0500

    [77] Assigning roles function

---
+++ src/Auctionify.Application/Common/Models/Account/AssignRoleToUserResponse.cs
@@ -0,0 +1,10 @@
+﻿using Auctionify.Application.Common.Models.BaseModels;
+using Auctionify.Core.Entities;
+
+namespace Auctionify.Application.Common.Models.Account
+{
+    public class AssignRoleToUserResponse : BaseResponse
+    {
+        public Role Role { get; set; }
+    }
+}
---
+++ src/Auctionify.Application/Common/Models/Account/AssignRoleToUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Auctionify.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Auctionify.Application.Common.Models.Account
+{
+    public class AssignRoleToUserViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public Role Role { get; set; }
+    }
+}
