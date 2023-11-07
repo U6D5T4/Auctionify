09f01a554d613725b8dd63a2ee6699e5c49edf43
otash8110 <otash_baxadirov@mail.ru>
2023-11-07 20:17:10 +0500

    [82] - refactor and redesign dialog popup

--- src/Auctionify.API/ClientApp/src/app/ui-elements/dialog-popup/dialog-popup.component.ts
+++ src/Auctionify.API/ClientApp/src/app/ui-elements/dialog-popup/dialog-popup.component.ts
@@ -9,16 +9,21 @@ export interface DialogData {
 @Component({
   selector: 'app-dialog-popup',
   templateUrl: './dialog-popup.component.html',
-  styleUrls: ['./dialog-popup.component.scss']
+  styleUrls: ['./dialog-popup.component.scss'],
 })
 export class DialogPopupComponent {
-  iconPath: string = "";
-  message: string = "";
-  constructor(public dialogRef: DialogRef<string>, @Inject(DIALOG_DATA) public data: DialogData) {
-    this.message = data.text[0];
-    this.iconPath = data.isError ?
-    "../../../assets/icons/alert-hexagon.svg" :
-    "../../../assets/icons/warning_triangle.svg";
+  iconPath: string = '';
+  messages: string[] = [];
+  isError: boolean = false;
+  constructor(
+    public dialogRef: DialogRef<string>,
+    @Inject(DIALOG_DATA) public data: DialogData
+  ) {
+    this.messages = data.text;
+    this.iconPath = data.isError
+      ? '../../../assets/icons/alert-hexagon.svg'
+      : '../../../assets/icons/success.svg';
+    this.isError = data.isError;
   }
 
   closeDialog() {
