c2f67938da3ef1afc6242e38d9bf3a79087b6ee3
otash8110 <otash_baxadirov@mail.ru>
2023-11-07 20:21:31 +0500

    [82] - refactor login

--- src/Auctionify.API/ClientApp/src/app/api-authorization/login/login.component.ts
+++ src/Auctionify.API/ClientApp/src/app/api-authorization/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injectable } from '@angular/core';
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthorizeService } from '../authorize.service';
 import { Dialog } from '@angular/cdk/dialog';
 import { DialogPopupComponent } from 'src/app/ui-elements/dialog-popup/dialog-popup.component';
@@ -7,23 +7,25 @@ import { LoginResponse } from 'src/app/web-api-client';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   isLoading = false;
 
-  constructor(private authService: AuthorizeService, public dialog: Dialog, private router: Router) {
-
-  }
+  constructor(
+    private authService: AuthorizeService,
+    public dialog: Dialog,
+    private router: Router
+  ) {}
   loginForm = new FormGroup({
-    email:  new FormControl<string>('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  })
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
   onSubmit() {
     this.isLoading = true;
@@ -32,30 +34,32 @@ export class LoginComponent {
       return;
     }
 
-    this.authService.login(
-      this.loginForm.controls.email.value!,
-      this.loginForm.controls.password.value!)
+    this.authService
+      .login(
+        this.loginForm.controls.email.value!,
+        this.loginForm.controls.password.value!
+      )
       .subscribe({
         next: (result) => {
-          this.router.navigate(['/home'])
+          this.router.navigate(['/home']);
         },
         error: (error: LoginResponse) => {
           this.openDialog(error.errors!, true);
-        }
-      })
+        },
+      });
   }
 
   openDialog(text: string[], error: boolean) {
     const dialogRef = this.dialog.open<string>(DialogPopupComponent, {
       data: {
         text,
-        isError: error
+        isError: error,
       },
     });
 
     dialogRef.closed.subscribe((res) => {
       this.isLoading = false;
       this.loginForm.controls.password.reset();
-    })
+    });
   }
 }
