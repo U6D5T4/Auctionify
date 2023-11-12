25b31cce2188af136093d9c77fd0bd792c6ad81d
Khasan Rashidov <khasanrashidov2002@gmail.com>
2023-11-12 20:59:26 +0500

    [118] - code refactoring

--- src/Auctionify.Core/Common/BaseAuditableEntity.cs
+++ src/Auctionify.Core/Common/BaseAuditableEntity.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Auctionify.Core.Common
+﻿namespace Auctionify.Core.Common
 {
-    public class BaseAuditableEntity : BaseEntity
-    {
-        public DateTime CreationDate { get; set; }
-        public DateTime ModificationDate { get; set; }
-    }
+	public class BaseAuditableEntity : BaseEntity
+	{
+		public DateTime CreationDate { get; set; }
+		public DateTime ModificationDate { get; set; }
+	}
 }
