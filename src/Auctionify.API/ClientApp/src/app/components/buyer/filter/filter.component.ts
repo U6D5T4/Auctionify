293b1a80277a106551554ae31de4d18c454aa6b6
otash8110 <otash_baxadirov@mail.ru>
2023-11-22 16:53:39 +0500

    [107] - implement OnInit, change const routing name,

--- src/Auctionify.API/ClientApp/src/app/components/buyer/filter/filter.component.ts
+++ src/Auctionify.API/ClientApp/src/app/components/buyer/filter/filter.component.ts
@@ -1,5 +1,5 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
-import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AppLocation, Category, Client, Status } from 'src/app/web-api-client';
 
@@ -24,7 +24,7 @@ interface FilterParameters {
     templateUrl: './filter.component.html',
     styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
     checkboxValues = new FormGroup({
         Closed: new FormControl<boolean>(false),
         Active: new FormControl<boolean>(false),
@@ -47,7 +47,9 @@ export class FilterComponent {
         public dialogRef: DialogRef<string>,
         @Inject(DIALOG_DATA) public data: any,
         private client: Client
-    ) {
+    ) {}
+
+    ngOnInit(): void {
         this.populateCategorySelector();
         this.populateLocations();
         this.populateLotStatuses();
