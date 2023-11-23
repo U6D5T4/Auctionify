f5824938d3e0e11beef556afcfd8e0cc5d1fa0fe
SaidganiUsm <saidgani110@gmail.com>
2023-11-23 18:43:14 +0500

    [94] Lots items fix

--- src/Auctionify.API/ClientApp/src/app/components/buyer/auction/auction.component.ts
+++ src/Auctionify.API/ClientApp/src/app/components/buyer/auction/auction.component.ts
@@ -103,4 +103,19 @@ export class AuctionComponent implements OnInit{
     const timeDifference = end.getTime() - start.getTime();
     return Math.ceil(timeDifference / (1000 * 3600 * 24));
   }
+
+  calculateDaysLeftUpcomming(startDate: Date | null): number | null {
+    if (!startDate) {
+      return null;
+    }
+
+    const now = new Date();
+    const start = new Date(startDate);
+    if (start <= now) {
+      return 0;
+    }
+
+    const timeDifference = start.getTime() - now.getTime();
+    return Math.ceil(timeDifference / (1000 * 3600 * 24));
+  }
 }
