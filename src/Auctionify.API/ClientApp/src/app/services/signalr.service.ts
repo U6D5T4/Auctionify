b3b8a03d67b18ee72cecd86e2bfb5bed2806f8f0
Khasan Rashidov <khasanrashidov2002@gmail.com>
2023-11-25 11:44:18 +0500

    [118] - signalr automatic reconnect setup

--- src/Auctionify.API/ClientApp/src/app/services/signalr.service.ts
+++ src/Auctionify.API/ClientApp/src/app/services/signalr.service.ts
@@ -14,6 +14,7 @@ export class SignalRService {
         this.connection = new signalR.HubConnectionBuilder()
             .configureLogging(signalR.LogLevel.Information)
             .withUrl(this.apiUrl + 'api/auctionHub')
+            .withAutomaticReconnect()
             .build();
 
         this.startConnection();
