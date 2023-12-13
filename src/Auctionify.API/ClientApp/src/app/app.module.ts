2bc73d1fe91511d4ab5782b4f4d16fd3dfc373fb
Khasan Rashidov <khasanrashidov2002@gmail.com>
2023-12-13 18:18:52 +0500

    [134] - app module changes

--- src/Auctionify.API/ClientApp/src/app/app.module.ts
+++ src/Auctionify.API/ClientApp/src/app/app.module.ts
@@ -8,15 +8,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ApiAuthorizationModule } from './api-authorization/api-authorization.module';
 import { AuthorizeInterceptor } from './api-authorization/authorize.interceptor';
-import { DashboardComponent } from './components/seller/dashboard/dashboard.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SellerModule } from './components/seller/seller.module';
 import { LayoutModule } from './layout/layout.module';
-import { BuyerModule } from './components/buyer/buyer.module';
 import { GeneralModule } from './components/general/general.module';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, DashboardComponent],
+    declarations: [AppComponent, HomeComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
