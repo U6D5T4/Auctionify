6f0271258cdd09b2e847b4ddf5ab803a443dfb2d
otash8110 <otash_baxadirov@mail.ru>
2024-01-07 17:59:55 +0500

    [32] - Implement GetAllLotsWithStatus for sellers and unit tests

---
+++ src/Auctionify.Application/Features/Lots/Queries/GetAllActiveForSeller/GetAllActiveLotsForSellerResponse.cs
@@ -0,0 +1,8 @@
+﻿using Auctionify.Application.Features.Lots.Queries.BaseQueryModels;
+
+namespace Auctionify.Application.Features.Lots.Queries.GetAllActiveForSeller
+{
+	public class GetAllLotsWithStatusForSellerResponse : GetAllLots
+	{
+	}
+}
