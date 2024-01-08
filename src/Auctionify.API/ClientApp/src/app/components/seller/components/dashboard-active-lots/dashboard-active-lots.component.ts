5ded2869c4b8c7104357e64aa6087298a14e7aa3
otash8110 <otash_baxadirov@mail.ru>
2024-01-08 15:49:00 +0500

    [32] - fixes to dashboard component and refactor

--- src/Auctionify.API/ClientApp/src/app/components/seller/components/dashboard-active-lots/dashboard-active-lots.component.ts
+++ src/Auctionify.API/ClientApp/src/app/components/seller/components/dashboard-active-lots/dashboard-active-lots.component.ts
@@ -20,21 +20,12 @@ import { SwiperContainer } from 'swiper/element';
     styleUrls: ['./dashboard-active-lots.component.scss'],
 })
 export class DashboardActiveLotsComponent implements OnInit {
-    @ViewChild('swiper', { static: true }) swiperEl!: ElementRef;
     activeLots: FilteredLotModel[] = [];
     filterResponse!: FilterResponse;
 
     constructor(private apiClient: Client) {}
 
     ngOnInit(): void {
-        const swiper = this.swiperEl.nativeElement as SwiperContainer;
-        const swiperParams = {
-            slidesPerView: 'auto',
-        };
-
-        Object.assign(swiper, swiperParams);
-        swiper.initialize();
-
         const pageRequest: PageRequest = {
             PageIndex: 0,
             PageSize: 100,
