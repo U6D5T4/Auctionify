9dd6f370fbd7dd771f352b0f2e8008d5d5845a46
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-01-12 01:03:09 +0500

    [33] - endpoint seting up (in progress)

---
+++ src/Auctionify.Application/Features/Users/Queries/GetTransactions/GetTransactionsUserQuery.cs
@@ -0,0 +1,67 @@
+﻿using Auctionify.Application.Common.DTOs;
+using Auctionify.Application.Common.Interfaces;
+using Auctionify.Application.Common.Interfaces.Repositories;
+using Auctionify.Application.Common.Models.Requests;
+using Auctionify.Core.Entities;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auctionify.Application.Features.Users.Queries.GetTransactions
+{
+	public class GetTransactionsUserQuery
+		: IRequest<GetListResponseDto<GetTransactionsUserResponse>>
+	{
+		public PageRequest PageRequest { get; set; }
+	}
+
+	public class GetTransactionsUserQueryHandler
+		: IRequestHandler<GetTransactionsUserQuery, GetListResponseDto<GetTransactionsUserResponse>>
+	{
+		private readonly ILotRepository _lotRepository;
+		private readonly IMapper _mapper;
+		private readonly IPhotoService _photoService;
+		private readonly ICurrentUserService _currentUserService;
+		private readonly UserManager<User> _userManager;
+		private readonly IBidRepository _bidRepository;
+
+		public GetTransactionsUserQueryHandler(
+			ILotRepository lotRepository,
+			IMapper mapper,
+			IPhotoService photoService,
+			ICurrentUserService currentUserService,
+			UserManager<User> userManager,
+			IBidRepository bidRepository
+		)
+		{
+			_lotRepository = lotRepository;
+			_mapper = mapper;
+			_photoService = photoService;
+			_currentUserService = currentUserService;
+			_userManager = userManager;
+			_bidRepository = bidRepository;
+		}
+
+		public async Task<GetListResponseDto<GetTransactionsUserResponse>> Handle(
+			GetTransactionsUserQuery request,
+			CancellationToken cancellationToken
+		)
+		{
+			var user = await _userManager.FindByEmailAsync(_currentUserService.UserEmail!);
+			var role = (await _userManager.GetRolesAsync(user!)).FirstOrDefault()!;
+
+
+
+			//if (role == "Buyer")
+			//{
+
+			//}
+			//else if (role == "Seller")
+			//{
+
+			//}
+
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Auctionify.Application/Features/Users/Queries/GetTransactions/GetTransactionsUserResponse.cs
@@ -0,0 +1,11 @@
+﻿using Auctionify.Application.Features.Lots.Queries.BaseQueryModels;
+
+namespace Auctionify.Application.Features.Users.Queries.GetTransactions
+{
+	public class GetTransactionsUserResponse : GetAllLots
+	{
+		public DateTime TransactionDate { get; set; }
+		public string TransactionStatus { get; set; }
+		public decimal TransactionAmount { get; set; }
+	}
+}
