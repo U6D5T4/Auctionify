36f87c822da7cad5770fc5a167ad66f60f07dc97
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-01-19 01:44:45 +0500

    [33] - transactions statuses refactoring

--- src/Auctionify.API/ClientApp/src/app/components/general/transactions/transactions.component.ts
+++ src/Auctionify.API/ClientApp/src/app/components/general/transactions/transactions.component.ts
@@ -1,12 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Client, TransactionModel } from 'src/app/web-api-client';
 
+enum TransactionStatus {
+    Withdraw = 'Withdraw',
+    Cancelled = 'Cancelled',
+    Winner = 'Winner',
+    Sold = 'Sold',
+    Loss = 'Loss',
+    Expired = 'Expired',
+}
+
 @Component({
     selector: 'app-transactions',
     templateUrl: './transactions.component.html',
     styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent implements OnInit {
+    private readonly MAIN_GRAY_COLOR: string = '#828282';
+    private readonly MAIN_RED_COLOR: string = '#ab2d25';
+    private readonly MAIN_GREEN_COLOR: string = '#2b9355';
+    private readonly DEFAULT_BLACK_COLOR: string = '#000000';
+
     @Input() transactionsCount: number = 100; // default to 100
     @Input() showHeader: boolean = true; // Default to true
 
@@ -24,5 +38,29 @@ export class TransactionsComponent implements OnInit {
             .subscribe((response: any) => {
                 this.transactions = response.items;
             });
+    }
+
+    getStatusColor(status: TransactionStatus | string | null): string {
+        if (status === null) {
+            return this.DEFAULT_BLACK_COLOR;
+        }
+
+        if (typeof status === 'string') {
+            status = status as TransactionStatus;
+        }
+
+        switch (status) {
+            case TransactionStatus.Withdraw:
+            case TransactionStatus.Cancelled:
+                return this.MAIN_GRAY_COLOR;
+            case TransactionStatus.Winner:
+            case TransactionStatus.Sold:
+                return this.MAIN_GREEN_COLOR;
+            case TransactionStatus.Loss:
+            case TransactionStatus.Expired:
+                return this.MAIN_RED_COLOR;
+            default:
+                return this.DEFAULT_BLACK_COLOR;
+        }
     }
 }
