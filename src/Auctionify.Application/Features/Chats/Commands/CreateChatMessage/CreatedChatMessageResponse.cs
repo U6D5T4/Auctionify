8d563aab977c5af3837485966df4e32cc55a36e7
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-01-25 13:50:26 +0500

    [152] - create chat message response corrections

--- src/Auctionify.Application/Features/Chats/Commands/CreateChatMessage/CreatedChatMessageResponse.cs
+++ src/Auctionify.Application/Features/Chats/Commands/CreateChatMessage/CreatedChatMessageResponse.cs
@@ -3,15 +3,5 @@
 	public class CreatedChatMessageResponse
 	{
 		public int Id { get; set; }
-
-		public int SenderId { get; set; }
-
-		public int ConversationId { get; set; }
-
-		public string Body { get; set; }
-
-		public bool IsRead { get; set; }
-
-		public DateTime TimeStamp { get; set; }
 	}
 }
