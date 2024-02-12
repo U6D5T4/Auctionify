49c1d4af68cf0ff71b12893d4001aabf62e93ec2
Otabek Bokhodirov <o.bahadirov@DESKTOP-QGI6G6J>
2024-02-12 20:33:12 +0500

    Add isUserPro guard

--- src/Auctionify.API/ClientApp/src/app/components/general/pro-subscribtion/subscription-routing.module.ts
+++ src/Auctionify.API/ClientApp/src/app/components/general/pro-subscribtion/subscription-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProPageComponent } from './pro-page/pro-page.component';
 import { UpgradeToProComponent } from './upgrade-to-pro/upgrade-to-pro.component';
+import { isProGuard } from 'src/app/guards/seller/is-pro.guard';
 
 const subscriptionRoutes: Routes = [
     {
         path: 'pro-subscription',
         component: ProPageComponent,
         pathMatch: 'full',
+        canActivate: [isProGuard],
     },
     {
         path: 'upgrade-to-pro',
