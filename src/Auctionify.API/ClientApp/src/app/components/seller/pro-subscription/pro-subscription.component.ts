56d58f6157aa00c747982bec6ee59b7f825ec0e9
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-02-13 19:52:30 +0500

    [168] - modal component setup (done)

--- src/Auctionify.API/ClientApp/src/app/components/seller/pro-subscription/pro-subscription.component.ts
+++ src/Auctionify.API/ClientApp/src/app/components/seller/pro-subscription/pro-subscription.component.ts
@@ -1,10 +1,6 @@
 import { Dialog } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthorizeService } from 'src/app/api-authorization/authorize.service';
-import { UserDataValidatorService } from 'src/app/services/user-data-validator/user-data-validator.service';
-import { ChoicePopupComponent } from 'src/app/ui-elements/choice-popup/choice-popup.component';
-import { Client } from 'src/app/web-api-client';
+import { ConfirmProSubscriptionComponent } from '../confirm-pro-subscription/confirm-pro-subscription.component';
 
 @Component({
     selector: 'app-pro-subscription',
@@ -12,25 +8,11 @@ import { Client } from 'src/app/web-api-client';
     styleUrls: ['./pro-subscription.component.scss'],
 })
 export class ProSubscriptionComponent {
-    constructor(
-        private authorizeService: AuthorizeService,
-        private client: Client,
-        public dialog: Dialog,
-        private snackBar: MatSnackBar
-    ) {}
+    constructor(public dialog: Dialog) {}
 
-    // create custom modal component with pro like look
-    // confirmSubscription() {
-    //     const dialogRef = this.dialog.open(ChoicePopupComponent, {
-    //         data: {
-    //             text: ['Are you sure you want to subscribe to our Pro plan?'],
-    //             isError: true,
-    //             continueBtnText: 'Subscribe',
-    //             breakBtnText: 'Cancel',
-    //             continueBtnColor: 'primary',
-    //             breakBtnColor: 'warn',
-    //         },
-    //         autoFocus: false,
-    //     });
-    // }
+    openConfirmProSubscriptionDialog() {
+        const dialogRef = this.dialog.open(ConfirmProSubscriptionComponent, {
+            autoFocus: false,
+        });
+    }
 }
