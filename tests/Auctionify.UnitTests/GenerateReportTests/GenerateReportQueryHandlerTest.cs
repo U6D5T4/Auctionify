b0388920c5fe100a8bf3e8309fbfd351bfcd5481
SaidganiUsm <saidgani110@gmail.com>
2024-02-14 13:23:20 +0500

    [172] Test update

--- tests/Auctionify.UnitTests/GenerateReportTests/GenerateReportQueryHandlerTest.cs
+++ tests/Auctionify.UnitTests/GenerateReportTests/GenerateReportQueryHandlerTest.cs
@@ -79,25 +79,51 @@ namespace Auctionify.UnitTests.GenerateReportTests
 		}
 
 		[Fact]
-		public async Task Handle_XlsxRequestForNonExistingUser_ReturnsEmptyByteArray()
+		public async Task Handle_GivenValidXlsxRequest_ReturnsPdfByteArray()
 		{
-			// Arrange:
-			var query = new GenerateReportQuery { Format = "XLSX", MonthsDuration = 1 };
+			// Arrange
+			var user = new User
+			{
+				Id = 1,
+				Email = "test@example.com",
+				IsDeleted = false
+			};
+			var reportData = new ReportData
+			{
+				TotalItemsCount = 10,
+				SoldItemsCount = 5,
+				TotalSoldItemsValue = 1000
+			};
+
+			var byteArray = new byte[] { 0x01, 0x02, 0x03 };
+
+			var reportDataMock = new Mock<IReportDataRepository>();
+			var xlsxReportServiceMock = new Mock<IXlsxReportGeneratorService>();
+
+			reportDataMock
+				.Setup(x => x.GetReportDataAsync(It.IsAny<ReportRequest>()))
+				.ReturnsAsync(reportData);
+
+			xlsxReportServiceMock
+				.Setup(x => x.GenerateReportAsync(It.IsAny<ReportData>(), It.IsAny<User>()))
+				.ReturnsAsync(byteArray);
 
 			var handler = new GenerateReportHandler(
-				_reportDataRepositoryMock.Object,
+				reportDataMock.Object,
 				_currentUserService,
 				_userManager,
-				_xlsxReportGeneratorServiceMock.Object,
+				xlsxReportServiceMock.Object,
 				_pdfReportGeneratorServiceMock.Object
 			);
+
+			var query = new GenerateReportQuery { Format = "XLSX", MonthsDuration = 1 };
 
 			// Act
 			var result = await handler.Handle(query, CancellationToken.None);
 
 			// Assert
 			Assert.NotNull(result);
-			Assert.Empty(result);
+			Assert.Equal(byteArray, result);
 		}
 	}
 }
