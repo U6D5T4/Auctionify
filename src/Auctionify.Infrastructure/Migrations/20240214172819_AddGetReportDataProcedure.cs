11e1a13295ed13241d9aa0f1bf3005b053fcea7f
SaidganiUsm <saidgani110@gmail.com>
2024-02-20 17:08:46 +0500

    Revert "[172] Migration update"
    This reverts commit e96ea7fed30c5ca49245a1dccc93b4525886d52f.

--- src/Auctionify.Infrastructure/Migrations/20240214172819_AddGetReportDataProcedure.cs
+++ src/Auctionify.Infrastructure/Migrations/20240214172819_AddGetReportDataProcedure.cs
@@ -17,36 +17,40 @@ namespace Auctionify.Infrastructure.Migrations
                     BEGIN
                         DECLARE @StartDate DATETIME = DATEADD(MONTH, -@MonthsBack, GETDATE());
     
+                        -- Total count of all lots listed by the user
                         DECLARE @TotalLots INT = (
-                            SELECT ISNULL(COUNT(*), 0)
+                            SELECT COUNT(*)
                             FROM Lots
                             WHERE SellerId = @UserId
-                            AND Lots.CreationDate >= @StartDate
+                            AND Lots.CreationDate >= @StartDate -- Specify the table name before CreationDate
                         );
     
+                        -- Count of lots that have been sold
                         DECLARE @SoldLotsCount INT = (
-                            SELECT ISNULL(COUNT(*), 0)
+                            SELECT COUNT(*)
                             FROM Lots
                             INNER JOIN LotStatuses ON Lots.LotStatusId = LotStatuses.Id
                             WHERE SellerId = @UserId
                             AND LotStatuses.Name = 'Sold'
-                            AND Lots.CreationDate >= @StartDate
+                            AND Lots.CreationDate >= @StartDate -- Specify the table name before CreationDate
                         );
     
+                        -- Total sum of the highest bids for the sold lots
                         DECLARE @TotalSoldAmount DECIMAL(18,2) = (
-                            SELECT ISNULL(SUM(MaxBid), 0.00)
+                            SELECT SUM(MaxBid)
                             FROM (
-                                SELECT MAX(NewPrice) AS MaxBid
+                                SELECT MAX(NewPrice) AS MaxBid -- Replace 'Amount' with the actual column name for bid amount
                                 FROM Bids
                                 INNER JOIN Lots ON Bids.LotId = Lots.Id
                                 INNER JOIN LotStatuses ON Lots.LotStatusId = LotStatuses.Id
                                 WHERE Lots.SellerId = @UserId
                                 AND LotStatuses.Name = 'Sold'
-                                AND Lots.CreationDate >= @StartDate
+                                AND Lots.CreationDate >= @StartDate -- Specify the table name before CreationDate
                                 GROUP BY Bids.LotId
                             ) AS SoldLotsMaxBids
                         );
     
+                        -- Return the results
                         SELECT @TotalLots AS TotalLots, @SoldLotsCount AS SoldLotsCount, @TotalSoldAmount AS TotalSoldAmount;
                     END;
                     GO";
