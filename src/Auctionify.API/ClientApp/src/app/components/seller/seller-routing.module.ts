04f55a056001f19dafb703a519f5ae806e3fb7b0
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-02-24 22:26:07 +0500

    [186] - my active lots (seller) creation, seller dashboard fixes

--- src/Auctionify.API/ClientApp/src/app/components/seller/seller-routing.module.ts
+++ src/Auctionify.API/ClientApp/src/app/components/seller/seller-routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateLotComponent } from './create-lot/create-lot.component';
 import { NgModule } from '@angular/core';
+import { ActiveLotsComponent } from './active-lots/active-lots.component';
 
 const sellerRoutes: Routes = [
     {
@@ -21,6 +22,11 @@ const sellerRoutes: Routes = [
     {
         path: 'update-lot/:id',
         component: CreateLotComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'active-lots',
+        component: ActiveLotsComponent,
         pathMatch: 'full',
     },
 ];
