343493dc9a012967bf079d9375bcc449f8a02d12
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-02-26 02:01:43 +0500

    [34] - analytics page improvements, db context initializer corrections

--- src/Auctionify.API/ClientApp/src/app/components/seller/analytics/analytics.component.ts
+++ src/Auctionify.API/ClientApp/src/app/components/seller/analytics/analytics.component.ts
@@ -1,227 +1,386 @@
 import { Component, OnInit } from '@angular/core';
-import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexStroke, ApexTooltip, ApexDataLabels, ApexNonAxisChartSeries, ApexFill, ApexLegend, ApexPlotOptions, ApexResponsive, ApexTheme, ApexTitleSubtitle, ApexYAxis, ApexStates, ApexGrid } from 'ng-apexcharts';
+import {
+    ApexAxisChartSeries,
+    ApexChart,
+    ApexXAxis,
+    ApexStroke,
+    ApexTooltip,
+    ApexDataLabels,
+    ApexNonAxisChartSeries,
+    ApexFill,
+    ApexLegend,
+    ApexPlotOptions,
+    ApexResponsive,
+    ApexTheme,
+    ApexTitleSubtitle,
+    ApexYAxis,
+    ApexStates,
+    ApexGrid,
+    ApexAnnotations,
+} from 'ng-apexcharts';
 import { Client } from 'src/app/web-api-client';
 
+enum LotStatusColor {
+    Draft = '#2b5293',
+    PendingApproval = '#40639e',
+    Rejected = '#5575a9',
+    Upcoming = '#6b86b3',
+    Active = '#8097be',
+    Sold = '#95a9c9',
+    NotSold = '#aabad4',
+    Cancelled = '#bfcbdf',
+    Reopened = '#d5dce9',
+    Archive = '#eaeef4',
+}
+
+const lotStatusOrder = [
+    'Draft',
+    'PendingApproval',
+    'Rejected',
+    'Upcoming',
+    'Active',
+    'Sold',
+    'NotSold',
+    'Cancelled',
+    'Reopened',
+    'Archive',
+];
+
 export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-  stroke: ApexStroke;
-  tooltip: ApexTooltip;
-  dataLabels: ApexDataLabels;
+    series: ApexAxisChartSeries;
+    chart: ApexChart;
+    xaxis: ApexXAxis;
+    stroke: ApexStroke;
+    tooltip: ApexTooltip;
+    dataLabels: ApexDataLabels;
+    plotOptions: ApexPlotOptions;
+    annotations: ApexAnnotations;
+    grid: ApexGrid;
+    fill: ApexFill;
 };
 
 export type BarChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  dataLabels: ApexDataLabels;
-  plotOptions: ApexPlotOptions;
-  yaxis: ApexYAxis;
-  xaxis: ApexXAxis;
-  fill: ApexFill;
-  title: ApexTitleSubtitle;
-  states: ApexStates;
-  grid: ApexGrid
+    series: ApexAxisChartSeries;
+    chart: ApexChart;
+    dataLabels: ApexDataLabels;
+    plotOptions: ApexPlotOptions;
+    yaxis: ApexYAxis;
+    xaxis: ApexXAxis;
+    fill: ApexFill;
+    title: ApexTitleSubtitle;
+    states: ApexStates;
+    grid: ApexGrid;
 };
 
 export type PolarChartOptions = {
-  series: ApexNonAxisChartSeries;
-  chart: ApexChart;
-  responsive: ApexResponsive[];
-  labels: any;
-  theme: ApexTheme;
-  title: ApexTitleSubtitle;
-  fill: ApexFill,
-  yaxis: ApexYAxis,
-  stroke: ApexStroke,
-  legend: ApexLegend,
-  plotOptions: ApexPlotOptions,
-  tooltip: ApexTooltip,
-  dataLabels: ApexDataLabels
+    series: ApexNonAxisChartSeries;
+    chart: ApexChart;
+    responsive: ApexResponsive[];
+    labels: any;
+    theme: ApexTheme;
+    title: ApexTitleSubtitle;
+    fill: ApexFill;
+    yaxis: ApexYAxis;
+    stroke: ApexStroke;
+    legend: ApexLegend;
+    plotOptions: ApexPlotOptions;
+    tooltip: ApexTooltip;
 };
 
 @Component({
-  selector: 'app-analytics',
-  templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+    selector: 'app-analytics',
+    templateUrl: './analytics.component.html',
+    styleUrls: ['./analytics.component.scss'],
 })
 export class AnalyticsComponent implements OnInit {
-  public chartOptions: Partial<ChartOptions> | null;
-  public polarChartOptions: Partial<PolarChartOptions> | null;
-  public barChartOptions: Partial<BarChartOptions> | null;
+    public chartOptions: Partial<ChartOptions> | null;
+    public polarChartOptions: Partial<PolarChartOptions> | null;
+    public barChartOptions: Partial<BarChartOptions> | null;
 
-  public barPeriod: string = 'Total';
-  public barPeriodNum: number = 1;
+    public barPeriod: string = 'Total';
+    public barPeriodNum: number = 1;
 
-  public linePeriod: string = 'Total';
-  public linePeriodNum: number = 1;
+    public linePeriod: string = 'Total';
+    public linePeriodNum: number = 1;
 
-  constructor(private client: Client) {
-    this.chartOptions = null;
-    this.barChartOptions = null
-    this.polarChartOptions = null;
-  }
+    constructor(private client: Client) {
+        this.chartOptions = null;
+        this.barChartOptions = null;
+        this.polarChartOptions = null;
+    }
 
-  ngOnInit(): void {
-    this.constructBarChart();
-    this.constructPolarChart();
-    this.constructLineChart();
-  }
+    ngOnInit(): void {
+        this.constructBarChart();
+        this.constructPolarChart();
+        this.constructLineChart();
+    }
 
-  constructPolarChart() {
-    this.client.getLotsStatuses().subscribe({
-      next: (res) => {
-        this.polarChartOptions = {
-          series: res.sort((a, b) => a.count - b.count).map(d => d.count),
-          chart: {
-            width: '80%',
-            type: 'donut',
-            foreColor: '#000'
-          },
-          labels: res.sort((a, b) => a.count - b.count).map(d => d.status),
-          fill: {
-            colors: ['#2b5293', '#40639e', '#5575a9', '#6b86b3', '#8097be', '#95a9c9', '#aabad4', '#bfcbdf', '#d5dce9', '#eaeef4']
-          },
-          yaxis: {
-            show: false
-          },
-          legend: {
-            position: 'bottom',
-            markers: {
-              fillColors: ['#2b5293', '#40639e', '#5575a9', '#6b86b3', '#8097be', '#95a9c9', '#aabad4', '#bfcbdf', '#d5dce9', '#eaeef4']
-            }
-          },
-          tooltip: {
-            fillSeriesColor: false,
-            marker: {
-              show: false
-            }
-          }
-        };
-      }
-    })
-  }
+    constructPolarChart() {
+        this.client.getLotsStatuses().subscribe({
+            next: (res) => {
+                const totalCount = res.reduce(
+                    (acc, curr) => acc + curr.count,
+                    0
+                );
 
-  constructBarChart() {
-    this.client.getCreatedLotsCount(this.barPeriod, this.barPeriodNum)
-      .subscribe({
-        next: (res) => {
-          this.barChartOptions = {
-            series: [
-              {
-                name: "Created lots",
-                data: res.data.map(d => d.count)
-              }
-            ],
-            chart: {
-              width: '80%',
-              type: "bar"
+                this.polarChartOptions = {
+                    series: res
+                        .sort((a, b) => a.count - b.count)
+                        .map((d) => d.count),
+                    chart: {
+                        width: '80%',
+                        type: 'donut',
+                        foreColor: '#000',
+                        fontFamily: 'Onest',
+                    },
+                    labels: res
+                        .sort((a, b) => a.count - b.count)
+                        .map((d) => d.status),
+                    fill: {
+                        colors: lotStatusOrder.map(
+                            (status) =>
+                                LotStatusColor[
+                                    status as keyof typeof LotStatusColor
+                                ]
+                        ),
+                    },
+                    yaxis: {
+                        show: false,
+                    },
+                    legend: {
+                        position: 'right',
+                        markers: {
+                            fillColors: lotStatusOrder.map(
+                                (status) =>
+                                    LotStatusColor[
+                                        status as keyof typeof LotStatusColor
+                                    ]
+                            ),
+                        },
+                        fontSize: '16px',
+                        fontFamily: 'Onest',
+                    },
+                    tooltip: {
+                        fillSeriesColor: false,
+                        marker: {
+                            show: false,
+                        },
+                    },
+                    plotOptions: {
+                        pie: {
+                            donut: {
+                                labels: {
+                                    show: true,
+                                    name: {
+                                        offsetY: 10,
+                                    },
+                                    total: {
+                                        show: true,
+                                        showAlways: true,
+                                        fontSize: '36px',
+                                        fontFamily: 'Onest',
+                                        fontWeight: 600,
+                                        label: totalCount.toString() + ' lots',
+                                        color: '#000',
+                                        formatter: function (w) {
+                                            return '';
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                };
             },
-            plotOptions: {
-              bar: {
-                dataLabels: {
-                  position: "top" // top, center, bottom
-                }
-              }
-            },
-            dataLabels: {
-              enabled: true,
-              offsetY: -20,
-              style: {
-                fontSize: "12px",
-                colors: ["#304758"]
-              }
-            },
-            xaxis: {
-              categories: res.data.map(d => (new Date(d.date)).toDateString()),
-              position: "top",
-              axisBorder: {
-                show: false
-              },
-              axisTicks: {
-                show: false
-              },
-              crosshairs: {
-                fill: {
-                  type: "gradient",
-                  gradient: {
-                    colorFrom: "#D8E3F0",
-                    colorTo: "#BED1E6",
-                    stops: [0, 100],
-                    opacityFrom: 0.4,
-                    opacityTo: 0.5
-                  }
-                }
-              },
-              tooltip: {
-                enabled: true,
-                offsetY: -35
-              }
-            },
-            states: {
-              active: {
-                filter: {
-                  type: 'darken',
-                  value: 0.65
-                }
-              }
-            },
-            fill: {
-              colors: ['#3768BA']
-            },
-            yaxis: {
-              axisBorder: {
-                show: false
-              },
-              axisTicks: {
-                show: false
-              },
-              labels: {
-                show: false,
-                formatter: function (val) {
-                  return val + "%";
-                }
-              }
-            },
-            grid: {
-              show: false
-            }
-          };
-        }
-      })
-  }
+        });
+    }
 
-  constructLineChart() {
-    this.client.getUserIncome(this.linePeriod, this.linePeriodNum).subscribe({
-      next: (res) => {
-        this.chartOptions = {
-          series: [{
-            name: 'main',
-            data: res.map(d => d.amount)
-          }],
-          chart: {
-            type: "area",
-            toolbar: {
-              show: false,
-            },
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: "smooth"
-          },
-          xaxis: {
-            type: "datetime",
-            categories: res.map(d => (new Date(d.date)).toDateString())
-          },
-          tooltip: {
-            x: {
-              format: "dd/MM/yy HH:mm"
-            }
-          }
-        };
-      }
-    })
-  }
+    constructBarChart() {
+        this.client
+            .getCreatedLotsCount(this.barPeriod, this.barPeriodNum)
+            .subscribe({
+                next: (res) => {
+                    this.barChartOptions = {
+                        series: [
+                            {
+                                name: 'Created lots',
+                                data: res.data.map((d) => d.count),
+                            },
+                        ],
+                        chart: {
+                            width: '80%',
+                            type: 'bar',
+                            fontFamily: 'Onest',
+                        },
+                        plotOptions: {
+                            bar: {
+                                dataLabels: {
+                                    position: 'top',
+                                },
+                                borderRadius: 8,
+                            },
+                        },
+                        dataLabels: {
+                            enabled: true,
+                            style: {
+                                fontSize: '12px',
+                                colors: ['#fff'],
+                                fontFamily: 'Onest',
+                            },
+                        },
+                        xaxis: {
+                            categories: res.data.map((d) =>
+                                new Date(d.date).toLocaleDateString('en-US', {
+                                    day: 'numeric',
+                                    month: 'short',
+                                    year: 'numeric',
+                                })
+                            ),
+                            position: 'top',
+                            axisBorder: {
+                                show: false,
+                            },
+                            axisTicks: {
+                                show: false,
+                            },
+                            crosshairs: {
+                                fill: {
+                                    type: 'gradient',
+                                    gradient: {
+                                        colorFrom: '#D8E3F0',
+                                        colorTo: '#BED1E6',
+                                        stops: [0, 100],
+                                        opacityFrom: 0.4,
+                                        opacityTo: 0.5,
+                                    },
+                                },
+                            },
+                            tooltip: {
+                                enabled: true,
+                                offsetY: -35,
+                                style: {
+                                    fontFamily: 'Onest',
+                                },
+                            },
+                        },
+                        states: {
+                            active: {
+                                filter: {
+                                    type: 'darken',
+                                    value: 0.65,
+                                },
+                            },
+                        },
+                        fill: {
+                            colors: ['#3768BA'],
+                        },
+                        yaxis: {
+                            axisBorder: {
+                                show: false,
+                            },
+                            axisTicks: {
+                                show: false,
+                            },
+                            labels: {
+                                show: false,
+                                formatter: function (val) {
+                                    return val + ' lot(s)';
+                                },
+                                style: {
+                                    fontFamily: 'Onest',
+                                },
+                            },
+                        },
+                        grid: {
+                            show: false,
+                        },
+                    };
+                },
+            });
+    }
+
+    constructLineChart() {
+        this.client
+            .getUserIncome(this.linePeriod, this.linePeriodNum)
+            .subscribe({
+                next: (res) => {
+                    this.chartOptions = {
+                        series: [
+                            {
+                                name: 'Income',
+                                data: res.map((d) => ({
+                                    x: new Date(d.date).getTime(),
+                                    y: d.amount,
+                                })),
+                                color: '#8AADE8',
+                            },
+                        ],
+                        chart: {
+                            type: 'area',
+                            toolbar: {
+                                show: false,
+                            },
+                            fontFamily: 'Onest',
+                        },
+                        dataLabels: {
+                            enabled: false,
+                            style: {
+                                fontFamily: 'Onest',
+                            },
+                        },
+                        stroke: {
+                            curve: 'smooth',
+                            colors: ['#2B5293'],
+                        },
+                        xaxis: {
+                            type: 'datetime',
+                            categories: res.map((d) =>
+                                new Date(d.date).toDateString()
+                            ),
+                        },
+                        tooltip: {
+                            x: {
+                                format: 'dd/MM/yy HH:mm',
+                            },
+                        },
+                        grid: {
+                            show: false,
+                        },
+                        annotations: {
+                            points: res.map((d) => {
+                                return {
+                                    x: new Date(d.date).getTime(),
+                                    y: d.amount,
+                                    marker: {
+                                        size: 6,
+                                        fillColor: '#fff',
+                                        strokeColor: '#2B5293',
+                                        strokeWidth: 3,
+                                        fontFamily: 'Onest',
+                                    },
+                                    label: {
+                                        borderColor: '#2B5293',
+                                        style: {
+                                            color: '#fff',
+                                            background: '#2B5293',
+                                            fontFamily: 'Onest',
+                                        },
+                                    },
+                                };
+                            }),
+                        },
+                        fill: {
+                            type: 'gradient',
+                            gradient: {
+                                shadeIntensity: 1,
+                                inverseColors: false,
+                                opacityFrom: 0.8,
+                                opacityTo: 0.4,
+                                stops: [0, 100],
+                            },
+                        },
+                    };
+                },
+            });
+    }
 }
--- src/Auctionify.Application/Features/Reports/Query/GetUserIncome/GetUserIncomeQuery.cs
+++ src/Auctionify.Application/Features/Reports/Query/GetUserIncome/GetUserIncomeQuery.cs
@@ -1,5 +1,5 @@
-﻿using Auctionify.Application.Common.Interfaces.Repositories;
-using Auctionify.Application.Common.Interfaces;
+﻿using Auctionify.Application.Common.Interfaces;
+using Auctionify.Application.Common.Interfaces.Repositories;
 using Auctionify.Core.Enums;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -13,30 +13,66 @@ namespace Auctionify.Application.Features.Reports.Query.GetUserIncome
 		public int PeriodNumber { get; set; }
 	}
 
-	public class GetUserIncomeQueryHandler : IRequestHandler<GetUserIncomeQuery, IList<GetUserIncomeResponse>>
+	public class GetUserIncomeQueryHandler
+		: IRequestHandler<GetUserIncomeQuery, IList<GetUserIncomeResponse>>
 	{
 		private readonly ILotRepository _lotRepository;
 		private readonly ICurrentUserService _currentUserService;
 		private readonly IBidRepository _bidRepository;
 
-		public GetUserIncomeQueryHandler(ILotRepository lotRepository, ICurrentUserService currentUserService, IBidRepository bidRepository)
+		public GetUserIncomeQueryHandler(
+			ILotRepository lotRepository,
+			ICurrentUserService currentUserService,
+			IBidRepository bidRepository
+		)
 		{
 			_lotRepository = lotRepository;
 			_currentUserService = currentUserService;
 			_bidRepository = bidRepository;
 		}
 
-		public async Task<IList<GetUserIncomeResponse>> Handle(GetUserIncomeQuery request, CancellationToken cancellationToken)
+		public async Task<IList<GetUserIncomeResponse>> Handle(
+			GetUserIncomeQuery request,
+			CancellationToken cancellationToken
+		)
 		{
-			var query = _lotRepository.Query().Include(l => l.Seller).Include(l => l.Bids).Include(l => l.LotStatus)
-				.Where(l => l.Seller.Email == _currentUserService.UserEmail && l.LotStatus.Name == AuctionStatus.Sold.ToString());
+			var query = _lotRepository
+				.Query()
+				.Include(l => l.Seller)
+				.Include(l => l.Bids)
+				.Include(l => l.LotStatus)
+				.Where(
+					l =>
+						l.Seller.Email == _currentUserService.UserEmail
+						&& l.LotStatus.Name == AuctionStatus.Sold.ToString()
+				);
 
 			query = request.Period switch
 			{
-				AnalyticReportPeriod.Day => query.Where(l => l.CreationDate >= DateTime.UtcNow.AddDays(-request.PeriodNumber) && l.CreationDate <= DateTime.UtcNow),
-				AnalyticReportPeriod.Week => query.Where(l => l.CreationDate >= DateTime.UtcNow.AddDays(-(request.PeriodNumber * 7)) && l.CreationDate <= DateTime.UtcNow),
-				AnalyticReportPeriod.Month => query.Where(l => l.CreationDate >= DateTime.UtcNow.AddMonths(-request.PeriodNumber) && l.CreationDate <= DateTime.UtcNow),
-				AnalyticReportPeriod.Year => query.Where(l => l.CreationDate >= DateTime.UtcNow.AddYears(-request.PeriodNumber) && l.CreationDate <= DateTime.UtcNow),
+				AnalyticReportPeriod.Day
+					=> query.Where(
+						l =>
+							l.CreationDate >= DateTime.UtcNow.AddDays(-request.PeriodNumber)
+							&& l.CreationDate <= DateTime.UtcNow
+					),
+				AnalyticReportPeriod.Week
+					=> query.Where(
+						l =>
+							l.CreationDate >= DateTime.UtcNow.AddDays(-(request.PeriodNumber * 7))
+							&& l.CreationDate <= DateTime.UtcNow
+					),
+				AnalyticReportPeriod.Month
+					=> query.Where(
+						l =>
+							l.CreationDate >= DateTime.UtcNow.AddMonths(-request.PeriodNumber)
+							&& l.CreationDate <= DateTime.UtcNow
+					),
+				AnalyticReportPeriod.Year
+					=> query.Where(
+						l =>
+							l.CreationDate >= DateTime.UtcNow.AddYears(-request.PeriodNumber)
+							&& l.CreationDate <= DateTime.UtcNow
+					),
 				_ => query,
 			};
 
@@ -44,24 +80,26 @@ namespace Auctionify.Application.Features.Reports.Query.GetUserIncome
 
 			var highestBids = lots.Select(item =>
 			{
-				var highestBid = _bidRepository.Query()
-											   .Where(b => b.LotId == item.Id)
-											   .OrderBy(b => b.NewPrice)
-											   .LastOrDefault();
-				return new
-				{
-					LotId = item.Id,
-					HighestBid = highestBid?.NewPrice ?? 0
-				};
+				var highestBid = _bidRepository
+					.Query()
+					.Where(b => b.LotId == item.Id)
+					.OrderBy(b => b.NewPrice)
+					.LastOrDefault();
+				return new { LotId = item.Id, HighestBid = highestBid?.NewPrice ?? 0 };
 			});
 
-			var result = (from data in lots
-						  group data by data.EndDate.Date into grouped
-						  select new GetUserIncomeResponse
-						  {
-							  Date = grouped.Key,
-							  Amount = highestBids.Where(x => x.LotId == grouped.Last().Id).Select(x => x.HighestBid).FirstOrDefault()
-						  }).ToList();
+			var result = (
+				from data in lots
+				group data by data.EndDate.Date into grouped
+				select new GetUserIncomeResponse
+				{
+					Date = grouped.Key,
+					Amount = highestBids
+						.Where(x => x.LotId == grouped.Last().Id)
+						.Select(x => x.HighestBid)
+						.FirstOrDefault()
+				}
+			).ToList();
 
 			return result;
 		}
